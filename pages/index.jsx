0d32c01dd8c987b118b394a49a88a9327e4a373e
estebansant <estebansant30@gmail.com>
2023-05-22 09:13:31 -0300

    setting up basic routes in next

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar';
+
+const Index = () => {
+  return (
+    <div>
+        <Navbar />
+        <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+export default Index
