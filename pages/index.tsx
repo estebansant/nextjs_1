d7a2c7dc0e172dee4508c44e8a325e6eab360f17
estebansant <estebansant30@gmail.com>
2023-05-25 11:55:30 -0300

    linking api with page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import { useRouter } from 'next/router'
 
 const HomePage = () => {
+  const [productList, setProductList] = React.useState<TProduct[]>([])
+
+  const {
+    query: { id },
+  } = useRouter()
+
+  React.useEffect(() => {
+    if (id) {
+      window
+        .fetch('/api/avo/${id}')
+        .then((response) => response.json())
+        .then(({ data, length }) => {
+          setProductList(data)
+        })
+    }
+  }, [id])
+
   return (
     <div>
       <Navbar />
       <div>Platzi and Next.js!</div>
+      {productList.map((product, i) => {
+        return <h3 key={i}>{product?.name}</h3>
+      })}
     </div>
   )
 }
